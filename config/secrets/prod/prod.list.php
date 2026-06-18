4a2f72df4b3db7ff256079825b617ed1e6e413eb
Thomas <shaltv@protonmail.com>
2026-06-18 12:11:50 +0200

    Test deploy

--- config/secrets/prod/prod.list.php
+++ config/secrets/prod/prod.list.php
@@ -2,7 +2,18 @@
 
 return [
     'APP_SECRET' => null,
+    'DB_HOST_PROD' => null,
+    'DB_NAME_PROD' => null,
+    'DB_PASSWORD_PROD_RO' => null,
+    'DB_PASSWORD_PROD_RW' => null,
+    'DB_PORT_PROD' => null,
+    'DB_USER_PROD_RO' => null,
+    'DB_USER_PROD_RW' => null,
     'JWT_PASSPHRASE' => null,
     'JWT_PRIVATE_KEY' => null,
     'JWT_PUBLIC_KEY' => null,
+    'RMQ_HOST_PROD' => null,
+    'RMQ_PASSWORD_PROD' => null,
+    'RMQ_PORT_PROD' => null,
+    'RMQ_USER_PROD' => null,
 ];
